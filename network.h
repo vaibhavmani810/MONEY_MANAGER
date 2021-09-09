b72a0499431848ee06e054cb42f19576bba1a988
vaibhavmani810 <74287392+vaibhavmani810@users.noreply.github.com>
2021-09-09 12:46:26 +0530

    Update network.h

--- network.h
+++ network.h
@@ -1,1 +1,25 @@
+#include<vector>
+#include<map>
 
+#ifndef NETWORK_H
+#define NETWORK_H
+
+
+struct Transaction {
+	std::string donor;
+	std::string borrower;
+	int amount;
+};
+
+class Network {
+	std::vector<Transaction> transactions;
+	std::map<std::string, int> net_amount;
+public:
+	void addTransaction(Transaction s);
+
+	void optimiseTransaction();
+
+	void print();
+};
+
+#endif
